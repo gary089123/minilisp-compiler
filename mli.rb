5266715cac2d30f1ea5035cfbad428e51adab2b0
gary089123 <gary089123@yahoo.com.tw>
2017-06-02 01:10:37 +0800

    scanner

--- mli.rb
+++ mli.rb
@@ -1,1 +1,8 @@
+require './scanner'
+
+
+input = "(define y (+ 1 2 3))"
+my_lexer = Scanner.new
+output = my_lexer.lex(input)
+output.each{|lexer, token| p token}
 
---
+++ scanner.rb
@@ -0,0 +1,63 @@
+require 'lex'
+class Scanner < Lex::Lexer
+  tokens(
+      :NUMBER,
+      :PLUS,
+      :MINUS,
+      :TIMES,
+      :DIVIDE,
+      :MOD,
+      :LPAREN,
+      :RPAREN,
+      :EQUALS,
+      :ID,
+      :AND,
+      :OR,
+      :NOT,
+      :DEFINE,
+      :FUN,
+      :IF,
+      :BL,
+      :SL
+  )
+
+  # Regular expression rules for simple tokens
+  rule(:PLUS,   /\+/)
+  rule(:MINUS,  /\-/)
+  rule(:TIMES,  /\*/)
+  rule(:DIVIDE, /\//)
+  rule(:MOD,    /mod/)
+  rule(:LPAREN, /\(/)
+  rule(:RPAREN, /\)/)
+  rule(:EQUALS, /\=/)
+  rule(:ID,     /[a-z][a-z0-9\-]*/)
+  rule(:AND,    /and/)
+  rule(:OR,     /or/)
+  rule(:NOT,    /not/)
+  rule(:DEFINE, /define/)
+  rule(:FUN,    /fun/)
+  rule(:BL,     /\>/)
+  rule(:SL,     /\</)
+
+
+
+  # A regular expression rules with actions
+  rule(:NUMBER, /[0-9]+/) do |lexer, token|
+    token.value = token.value.to_i
+    token
+  end
+
+  # Define a rule so we can track line numbers
+  rule(:newline, /\n+/) do |lexer, token|
+    lexer.advance_line(token.value.length)
+  end
+
+  # A string containing ignored characters (spaces and tabs)
+  ignore " \t"
+
+  error do |lexer, token|
+    puts "Illegal character #{token.value}"
+  end
+end
+
+# build the lexer
